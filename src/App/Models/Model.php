289897b1bb84908fc98d67d0da576817cf645c1f
Vitor Veras <vitor.veras@secrel.net.br>
2018-09-26 18:26:21 -0300

    Inicia o esqueleto da classe Model.

--- src/App/Models/Model.php
+++ src/App/Models/Model.php
@@ -11,7 +11,12 @@ class Model
     public function __construct()
     {
         $this->config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/lib/config.ini');
+        $this->initializeDB();
         $this->connectDB();
+    }
+    public function initializeDB()
+    {
+        // inicializa o banco de dados de tarefas, com as colunas propostas
     }
     public function connectDB()
     {
@@ -21,6 +26,26 @@ class Model
         } catch (PDOException $exc) {
             echo $exc->getMessage();
         }
+    }
+    public function showAllTasks()
+    {
+        // READ: retorna toda tabela de tarefas
+    }
+    public function showTask($id)
+    {
+        // READ: retorna uma específica linha utilizando o $id
+    }
+    public function createTask($data)
+    {
+        // CREATE: recebe um array de dados da tarefa do controller e cria uma nova linha no banco com as informacoes.
+    }
+    public function deleteTask($id)
+    {
+        // DELETE: deleta uma tarefa específica do banco utilizando $id.
+    }
+    public function updateTask($id)
+    {
+        // UPDATE: atualiza uma tarefa específica utilizando o $id.
     }
 
 }
