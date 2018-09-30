56e9f9afcec82befee320bcbad6def1371a4558a
Vitor Veras <vitorverasm@gmail.com>
2018-09-30 15:09:39 -0300

    Implementa createTask no Model.

--- src/App/Models/Model.php
+++ src/App/Models/Model.php
@@ -11,14 +11,13 @@ class Model
     public function __construct()
     {
         $this->config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/lib/config.ini');
-        // $this->tableName = $this->config['tablename'];
         $this->conn = $this->connectDB();
         $this->initializeTable();
     }
     public function initializeTable()
     {
         $sql = "
-        CREATE TABLE IF NOT EXISTS {$this->config['tablename']} (
+        CREATE TABLE IF NOT EXISTS {$this->config['tablename']}(
             id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
             name VARCHAR(30) NOT NULL,
             description VARCHAR(100) NOT NULL,
@@ -38,7 +37,7 @@ class Model
     public function connectDB()
     {
         try {
-            $conn = new PDO("mysql:host=" . $this->config['host'] . ";dbname=" . $this->config['dbname'], $this->config['username'], $this->config['password']);
+            $conn = new PDO("mysql:host=" . $this->config['host'] . ";dbname=" . $this->config['dbname'] . ";charset=utf8", $this->config['username'], $this->config['password']);
             echo 'Conected!';
             return $conn;
         } catch (PDOException $exc) {
@@ -57,29 +56,22 @@ class Model
     {
         // CREATE: recebe um array de dados da tarefa do controller e cria uma nova linha no banco com as informacoes.
         $sql = "
-        INSERT INTO :table (name, description, deadline, priority, status)
-        VALUES (:name, :description, :deadline, :priority, :status)
+        INSERT INTO {$this->config['tablename']} (name, description, deadline, priority, status)
+        VALUES (?, ?, ?, ?, ?)
         ";
         try {
-            $pdo = $this->conn::getInstance();
-            $stmt = $pdo->prepare($sql);
+            $stmt = $this->conn->prepare($sql);
+            
+            // $stmt->bindParam(':name', $data['name'], PDO::PARAM_STR);
+            // $stmt->bindParam(':description', $data['description'], PDO::PARAM_STR);
+            // $stmt->bindParam(':deadline', $data['deadline'], PDO::PARAM_STR);
+            // $stmt->bindParam(':priority', $data['priority'], PDO::PARAM_INT);
+            // $stmt->bindParam(':status', $data['status'], PDO::PARAM_INT);
 
-            $a = $stmt->bindParam(':table', $this->config['tablename'], PDO::PARAM_STR);
-            var_dump($data['name']);
-            $b = $stmt->bindParam(':name', $data['name'], PDO::PARAM_STR);
-            var_dump($data['description']);
-            $c = $stmt->bindParam(':description', $data['description'], PDO::PARAM_STR);
-            var_dump($data['deadline']);
-            $d = $stmt->bindParam(':deadline', $data['deadline'], PDO::PARAM_STR);
-            var_dump($data['priority']);
-            $e = $stmt->bindParam(':priority', $data['priority'], PDO::PARAM_INT);
-            var_dump($data['status']);
-            $f = $stmt->bindParam(':status', $data['status'], PDO::PARAM_INT);
-            $stmt->execute();
-            echo "ok";
+            $stmt->execute([$data['name'], $data['description'], $data['deadline'], $data['priority'], $data['status']]);
+
         } catch (PDOException $e) {
-            echo $e->getMessage();
-            die();
+            if($e->errorInfo[1] === 1062) echo 'Duplicate entry';
         }
     }
     public function deleteTask($id)
