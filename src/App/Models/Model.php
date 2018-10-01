3e61d2d6de429f74ca60ca31f1462fadabdf1181
Vitor Veras <vitor.veras@secrel.net.br>
2018-10-01 18:46:39 -0300

    Implementa todas funcoes model.

--- src/App/Models/Model.php
+++ src/App/Models/Model.php
@@ -10,7 +10,6 @@ class Model
     private $config;
     public function __construct()
     {
-        var_dump('Chama construtor');
         $this->config = parse_ini_file($_SERVER['DOCUMENT_ROOT'] . '/lib/config.ini');
         $this->conn = $this->connectDB();
         $this->initializeTable();
@@ -29,7 +28,6 @@ class Model
         ";
         try {
             $stmt = $this->conn->prepare($sql);
-            var_dump('INITIALIZE TABLE');
             $stmt->execute();
         } catch (PDOException $e) {
             echo $e->getMessage();
@@ -48,23 +46,39 @@ class Model
     }
     public function showAllTasks()
     {
-        // READ: retorna toda tabela de tarefas
+        $sql = "SELECT * FROM {$this->config['tablename']};";
+        try {
+            $result = $this->conn->query($sql)->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $exc->getMessage();
+        }
+
     }
     public function showTask($id)
     {
-        // READ: retorna uma específica linha utilizando o $id
+        $sql = "SELECT * FROM {$this->config['tablename']} WHERE id = :id LIMIT 1";
+        try {
+            $stmt = $this->conn->prepare($sql);
+            $values = [
+                ':id' => $id
+            ];
+            $stmt->execute($values);
+            $result = $stmt->fetch();
+            return $result;
+        } catch (PDOException $e) {
+            echo $exc->getMessage();
+        }
     }
     public function createTask($data)
     {
-        var_dump('Chamou método');
-        // CREATE: recebe um array de dados da tarefa do controller e cria uma nova linha no banco com as informacoes.
         $sql = "
         INSERT INTO {$this->config['tablename']} (name, description, deadline, priority, status)
         VALUES (:name, :description, :deadline, :priority, :status)
         ";
+
         try {
             $stmt = $this->conn->prepare($sql);
-            
             $values = [
                 ':name' => $data['name'],
                 ':description' => $data['description'],
@@ -72,31 +86,42 @@ class Model
                 ':priority' => $data['priority'],
                 ':status' => $data['status'],
             ];
-
-            // $stmt->bindParam(':name', $data['name'], PDO::PARAM_STR);
-            // $stmt->bindParam(':description', $data['description'], PDO::PARAM_STR);
-            // $stmt->bindParam(':deadline', $data['deadline'], PDO::PARAM_STR);
-            // $stmt->bindParam(':priority', $data['priority'], PDO::PARAM_INT);
-            // $stmt->bindParam(':status', $data['status'], PDO::PARAM_INT);
-            echo "<pre>";
-            var_dump($values);
-            echo "</pre>";
-
-            if (!$stmt->execute($values)) {
-                print_r($stmt->errorInfo());
-            }  
-
+            $stmt->execute($values);
         } catch (PDOException $e) {
-            if($e->errorInfo[1] === 1062) echo 'Duplicate entry';
+            echo $exc->getMessage();
         }
     }
     public function deleteTask($id)
     {
-        // DELETE: deleta uma tarefa específica do banco utilizando $id.
-    }
-    public function updateTask($id)
-    {
-        // UPDATE: atualiza uma tarefa específica utilizando o $id.
-    }
+        $sql = "DELETE FROM {$this->config['tablename']} WHERE id=:id";
 
+        try {
+            $stmt = $this->conn->prepare($sql);
+            $values = [
+                ':id' => $id
+            ];
+            $stmt->execute($values);
+        } catch (PDOException $e) {
+            echo $exc->getMessage();
+        }
+    }
+    public function updateTask($id, $data)
+    {
+        $sql = "UPDATE {$this->config['tablename']} SET name=:name, description=:description, deadline=:deadline, priority=:priority, status=:status WHERE id=:id;";
+
+        try {
+            $stmt = $this->conn->prepare($sql);
+            $values = [
+                ':name' => $data['name'],
+                ':description' => $data['description'],
+                ':deadline' => $data['deadline'],
+                ':priority' => $data['priority'],
+                ':status' => $data['status'],
+                ':id' => $id
+            ];
+            $stmt->execute($values);
+        } catch (PDOException $e) {
+            echo $exc->getMessage();
+        }
+    }
 }
