90a5ed35447eb76922d29c85fac03a1848ff7c4f
Vitor Veras <vitor.veras@secrel.net.br>
2018-10-05 18:36:45 -0300

    inicio da pagina home do projeto.

--- index.php
+++ index.php
@@ -1,11 +1,8 @@
 <?php
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-use App\Controllers\Controller;
 
 require 'vendor/autoload.php';
+$str = "ola asdsa";
 
-$json = '{"name":"Almoço","description":"Almoço na casa da Ana.","deadline":"2018-10-02 12:00:00","priority":1, "status":0}';
-$c = new Controller();
-// $c->createTask($json);
-var_dump ($c->showAllTasks());
+require_once './index.html';
