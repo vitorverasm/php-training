0149ff2df639935d0d5120d0b84988ab571b2bf6
Vitor Veras <vitorverasm@gmail.com>
2018-10-15 21:33:30 -0300

    Inicia funcao de retorno de tarefas.

--- src/App/Controllers/TaskController.php
+++ src/App/Controllers/TaskController.php
@@ -2,10 +2,13 @@
 use App\Controllers\Controller;
 
 require '../../../vendor/autoload.php';
-if (!empty($_POST) && !empty($_POST['action']) && !empty($_POST['body'])) {
+
+if (!empty($_POST) && !empty($_POST['action'])) {
     $controller = new Controller();
     switch ($_POST['action']) {
         case 'create':
             $controller->createTask($_POST['body']);
+        case 'get':
+            echo json_encode($controller->showAllTasks());
     }
 }
